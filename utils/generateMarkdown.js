7c39e0395c07345af437295eb114de2821f21236
Angela Man <angela.k.man@gmail.com>
2021-05-04 21:56:49 -0500

    installed inquirer

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+  // add license badge to top of readme
+  return `
+  
+  `;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,6 +26,28 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+          ## Description
+          ${data.description}
+
+          ## Table of Contents
+          ${data.tableOfContents}
+
+          ## Installation
+          ${data.installation}
+
+          ## Usage
+          ${data.usage}
+
+          ## License
+
+          ## Contributing
+
+          ## Tests
+
+          ## Questions
+          Visit my GitHub profile for more information: ${gitHubLink}
+          You can reach me at ${email} with any additional questions
 
 `;
 }
