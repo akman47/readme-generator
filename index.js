b833f32d38e2d87756b7181386778d9d5a9a0026
Angela Man <angela.k.man@gmail.com>
2021-05-06 23:14:01 -0500

    generate markdown template

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const questions = [
     },
     {
         inputName: "usage",
-        inputMessage: "Provide instructions and examples of usage."
+        inputMessage: "Provide instructions and examples for use."
     },
     {
         inputName: "contributing",
@@ -56,106 +56,48 @@ const promptInput = questions.map( field => {
         }
     });
 
-// confirm Table of Contents
-const tableContents = 
+// gather license information
+const licensePrompt = 
     {
-        type: "confirm",
-        name: "confirmTable",
-        message: "Would you like to include a Table of Contents?",
-        default: true
+        type: "checkbox",
+        name: "license",
+        message: "Which license(s) apply to the project?",
+        choices: ["",""]
     };
 
-promptInput.push(tableContents);
+promptInput.push(licensePrompt);
 
-
-const promptUser = () => {
-    return inquirer.prompt (promptInput).then(answers =>(console.log(answers)));
-}
-
-promptUser();
-
-// const promptUser = () => {
-//     return inquirer.prompt([
-//         {
-//             type: "input",
-//             name: "title",
-//             message: "What is the title of your project? (Required)",
-//             validate: titleInput => {
-//                 if (titleInput) {
-//                     return true;
-//                 }
-//                 else {
-//                     console.log("Please enter a project title");
-//                     return false;
-//                 }
-//             }
-//         },
-//         {
-//             type: "input",
-//             name: "description",
-//             message: "Provide a description of the project (Required)",
-//             validate: descriptionInput => {
-//                 if (descriptionInput) {
-//                     return true;
-//                 }
-//                 else {
-//                     console.log("Please enter a project description");
-//                     return false;
-//                 }
-//             }
-//         },
-//         {
-//             type: "confirm",
-//             name: "confirmTable",
-//             message: "Would you like to include a Table of Contents?",
-//             default: true
-//         },
-//         {
-//             type: "input",
-//             name: "installation",
-//             message: "Provide the steps required to install your project"
-//         },
-//         {
-//             type: "input",
-//             name: "usage",
-//             message: "Provide instructions and examples for use"
-//         },
-//         {
-//             type: "input",
-//             name: "contributing",
-//             message: "Provide guidelines on how other developers can contribute to this project"
-//         },
-//         {
-//             type: "checkbox",
-//             name: "licenses",
-//             message: "",
-//             choices: ["",""]
-//         },
-//         {
-//             type: "input",
-//             name: "tests",
-//             message: "Provide tests for the application and examples on how to run them"
-//         },
-//         {
-//             type: "input",
-//             name: "github",
-//             message: "What is your GitHub user name?"
-//         },
-//         {
-//             type: "input",
-//             name: "email",
-//             message: "What is your email address?"
-//         }
-
-//     ])
-//     .then(answers => console.log(answers));
-// }
+const fileName = data => "./dist/" + data.title + "_README.md"
 
 // TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve ({
+                ok: true,
+                message: "New README.md has been created!"
+            });
+        });
+    });
+}
 
 // TODO: Create a function to initialize app
-//function init() {}
+function init() {
+    console.log(
+        `
+        ==================================
+         Provide details for a new README
+        ==================================
+        `
+    );
+    // start question prompt
+    return inquirer.prompt (promptInput).then(answers =>(console.log(answers)));
+
+}
 
 // Function call to initialize app
 //init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,7 +6,7 @@ function renderLicenseBadge(license) {
   }
   // add license badge to top of readme
   return `
-  
+    ${license.badge}
   `;
 }
 
@@ -17,11 +17,25 @@ function renderLicenseLink(license) {
     return "";
   }
 
+  return `
+  Please refer to ${license.link} for the full terms.
+  `
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+
+  return `
+    ## License
+    ${renderLicenseBadge}
+    This application is licensed under the terms of the ${data.license} open source license.
+    ${renderLicenseLink}
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -44,8 +58,7 @@ function generateMarkdown(data) {
           ## Usage
           ${data.usage}
 
-          ## License
-          This application is covered under ${data.license}
+          ${renderLicenseSection}
 
           ## Contributing
           ${data.contribution}
